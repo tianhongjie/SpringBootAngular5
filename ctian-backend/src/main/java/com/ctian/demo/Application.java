99844d3e7462259311b57905c4cfd33fe1147594
Charlie Tian <charlie.tian@spsoft-cn.com>
2018-04-26 15:24:58 +0800

    [ADD] Init Project spring boot with angular

---
+++ ctian-backend/src/main/java/com/ctian/demo/Application.java
@@ -0,0 +1,14 @@
+package com.ctian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Charlie Tian
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
